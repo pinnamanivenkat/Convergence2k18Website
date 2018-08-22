54d8a8bc17029aff0a6ee3866d0d4fd5e32c22f6
pinnamanivenkat <pinnamanivenkat@gmail.com>
2018-08-22 09:18:51 +0530

    Animated preloader

--- js/main.js
+++ js/main.js
@@ -1,20 +1,164 @@
 (function($) {
+    $('.convergence-text').hide();
     var animData = {
-        wrapper: document.getElementById('preloader'),
+        wrapper: document.getElementById('preloader-animation'),
         animType: 'svg',
+        loop: true,
         prerender: true,
-        loop: false,
         autoplay: true,
-        path: 'drop_loading.json'
+        path: 'animation.json'
     };
     var anim = bodymovin.loadAnimation(animData);
+    anim.setSpeed(1);
+    bodymovin.setSubframeRendering(false);
 
-    anim.addEventListener('complete',function() {
-        if(document.readyState === "complete" || document.readyState === "interactive") {
+    $(window).on('load', function() {
+        setTimeout(function() {
             anim.stop();
-            $('#preloader').hide('fast');
-        } else {
-            anim.play();
-        }
-    })
+            setTimeout(function() {
+                $('#preloader').fadeOut('fast');
+            }, 1700);
+            $('#preloader-animation').hide();
+            $('.convergence-text').show();
+            $('.ml1 .letters').each(function() {
+                $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+            });
+            anime.timeline({
+                    loop: false
+                })
+                .add({
+                    targets: '.ml1 .letter',
+                    scale: [0.3, 1],
+                    opacity: [0, 1],
+                    translateZ: 0,
+                    easing: "easeOutExpo",
+                    duration: 600,
+                    delay: function(el, i) {
+                        return 70 * (i + 1)
+                    }
+                }).add({
+                    targets: '.ml1 .line',
+                    scaleX: [0, 1],
+                    opacity: [0.5, 1],
+                    easing: "easeOutExpo",
+                    duration: 700,
+                    offset: '-=875',
+                    delay: function(el, i, l) {
+                        return 80 * (l - i);
+                    }
+                });
+        }, 6000);
+    });
+
+    particlesJS("particles-js", {
+        "particles": {
+            "number": {
+                "value": 100,
+                "density": {
+                    "enable": true,
+                    "value_area": 800
+                }
+            },
+            "color": {
+                "value": "#ffffff"
+            },
+            "shape": {
+                "type": "circle",
+                "stroke": {
+                    "width": 0,
+                    "color": "#000000"
+                },
+                "polygon": {
+                    "nb_sides": 5
+                },
+                "image": {
+                    "src": "img/github.svg",
+                    "width": 100,
+                    "height": 100
+                }
+            },
+            "opacity": {
+                "value": 1,
+                "random": false,
+                "anim": {
+                    "enable": false,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 3,
+                "random": true,
+                "anim": {
+                    "enable": false,
+                    "speed": 40,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": false,
+                "distance": 150,
+                "color": "#ffffff",
+                "opacity": 0.4,
+                "width": 1
+            },
+            "move": {
+                "enable": true,
+                "speed": 2,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false,
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "grab"
+                },
+                "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                },
+                "resize": true
+            },
+            "modes": {
+                "grab": {
+                    "distance": 150,
+                    "line_linked": {
+                        "opacity": 0
+                    }
+                },
+                "bubble": {
+                    "distance": 400,
+                    "size": 40,
+                    "duration": 2,
+                    "opacity": 8,
+                    "speed": 3
+                },
+                "repulse": {
+                    "distance": 200,
+                    "duration": 0.4
+                },
+                "push": {
+                    "particles_nb": 8
+                },
+                "remove": {
+                    "particles_nb": 2
+                }
+            }
+        },
+        "retina_detect": true
+    });
+
 })(jQuery);
