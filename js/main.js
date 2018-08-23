e409d37200b76842bf36dff2dba29c4adfd59266
pinnamanivenkat <pinnamanivenkat@gmail.com>
2018-08-23 15:07:45 +0530

    Scroll spy along with menu overlay
    Signed-off-by: pinnamanivenkat <pinnamanivenkat@gmail.com>

--- js/main.js
+++ js/main.js
@@ -7,6 +7,38 @@
         $(".menu-overlay").toggleClass("open");
     });
 
+    $('a').not('[href="#"]').not('[href="#0"]').click(function (event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('.menu').toggleClass('open');
+                $('.menu-overlay').toggleClass('open');
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, "swing", function () {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
 
     var animData = {
         wrapper: document.getElementById('preloader-animation'),
