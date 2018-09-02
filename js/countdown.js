9bb6bb6b8b095361d16647332ee84077c7bfaf1d
pinnamanivenkat <pinnamanivenkat@gmail.com>
2018-09-02 18:30:32 +0530

    Set to convergence date

--- js/countdown.js
+++ js/countdown.js
@@ -1,45 +1,4 @@
-//function getTimeRemaining(endtime) {
-//  var t = Date.parse(endtime) - Date.parse(new Date());
-//  var seconds = Math.floor((t / 1000) % 60);
-//  var minutes = Math.floor((t / 1000 / 60) % 60);
-//  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-//  var days = Math.floor(t / (1000 * 60 * 60 * 24));
-//  return {
-//    'total': t,
-//    'days': days,
-//    'hours': hours,
-//    'minutes': minutes,
-//    'seconds': seconds
-//  };
-//}
-//
-//function initializeClock(id, endtime) {
-//  var clock = document.getElementById(id);
-//  var daysSpan = clock.querySelector('.days');
-//  var hoursSpan = clock.querySelector('.hours');
-//  var minutesSpan = clock.querySelector('.minutes');
-//  var secondsSpan = clock.querySelector('.seconds');
-//
-//  function updateClock() {
-//    var t = getTimeRemaining(endtime);
-//
-//    daysSpan.innerHTML = t.days;
-//    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-//    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-//    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-//
-//    if (t.total <= 0) {
-//      clearInterval(timeinterval);
-//    }
-//  }
-//
-//  updateClock();
-//  var timeinterval = setInterval(updateClock, 1000);
-//}
-//
-//var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
-//initializeClock('clockdiv', deadline);
-var target_date = new Date().getTime() + (1000*3600*48); // set the countdown date
+var target_date = new Date(2018,9,5,0,0,0);
 var days, hours, minutes, seconds; // variables for time units
 
 var countdown = document.getElementById("tiles"); // get tag element
@@ -56,18 +15,17 @@ function getCountdown(){
 
 	days = pad( parseInt(seconds_left / 86400) );
 	seconds_left = seconds_left % 86400;
-		 
+
 	hours = pad( parseInt(seconds_left / 3600) );
 	seconds_left = seconds_left % 3600;
-		  
+
 	minutes = pad( parseInt(seconds_left / 60) );
 	seconds = pad( parseInt( seconds_left % 60 ) );
 
 	// format countdown string + set tag value
-	countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>"; 
+	countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>";
 }
 
 function pad(n) {
 	return (n < 10 ? '0' : '') + n;
 }
-
